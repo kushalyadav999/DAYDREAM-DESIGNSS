87a57e8cd17a29534a7219255a81ab193ea9cd40
kushalyadav999 <awesomekushal108@gmail.com>
2025-11-30 19:13:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // ====== Firebase config (REPLACE these with your own Firebase project values) ======
 const firebaseConfig = {
-  apiKey: "<AIzaSyBqcFZdP284oYPscP6HX1Cg-UWGbQ3vhQs>",
-  authDomain: "<daydream-design>.firebaseapp.com",
-  projectId: "<daydream-design>",
-  storageBucket: "<daydream-design>.appspot.com",
-  messagingSenderId: "<670534738318>",
-  appId: "<1:670534738318:web:5b4ec59b826e13b3a684df>"
+  apiKey: "AIzaSyBqcFZdP284oYPscP6HX1Cg-UWGbQ3vhQs",
+  authDomain: "daydream-design.firebaseapp.com",
+  projectId: "daydream-design",
+  storageBucket: "daydream-design.appspot.com",
+  messagingSenderId: "670534738318",
+  appId: "1:670534738318:web:5b4ec59b826e13b3a684df"
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
